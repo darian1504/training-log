6c44a382a5b60d1252c1a59ccd915e28fb49b5b8
Serial Incubator <274148479+darian1504@users.noreply.github.com>
2026-04-23 15:50:56 +1000

    Bump cache to v2 to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-log-v1';
+const CACHE = 'training-log-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {

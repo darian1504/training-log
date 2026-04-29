04cd76b6dd74d7ea9484eb72acb491b70a07ac94
Serial Incubator <274148479+darian1504@users.noreply.github.com>
2026-04-30 09:04:39 +1000

    Bump cache to v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-log-v5';
+const CACHE = 'training-log-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {

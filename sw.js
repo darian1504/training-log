19f470ad7a155daef8a6f1a5fc35af0bcabc42ac
Serial Incubator <274148479+darian1504@users.noreply.github.com>
2026-05-05 10:37:57 +1000

    feat: freemium model, analytics, Secondary rename, bottom nav [sw.js]

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-log-v17';
+const CACHE = 'training-log-v18';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192px.png', './icon-512px.png'];
 
 self.addEventListener('install', e => {
